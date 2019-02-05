b62055378b3313f0cf8411de091e879210a6097d
h571544 <fredrik.1997@hotmail.com>
2019-02-05 12:02:41 +0100

    testMetoder

---
+++ Yatzoo/src_test/no/hvl/dat109/test/src_test.java
@@ -0,0 +1,14 @@
+package no.hvl.dat109.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class src_test {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
